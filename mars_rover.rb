c72d3d13c10d66dd92b1002a0399db895e6c8a4c
vijendra <vijendrakarkala@gmail.com>
2011-12-15 12:40:44 +0530

    getting the inputs from the user

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,24 +1,25 @@
 require File.expand_path("../init.rb", __FILE__)
 
-puts "Setting the plateau cordinate to 5,5"
-@plateau = Plateau.new('5 5')
+puts"Enter the upper-right coordinates of the plateau. Example 5 5"
+@plateau = Plateau.new(gets.chomp)
 
-puts "Adding a rover Rover_a with position 1 2 N to the plateau"
-@rover = Rover.new('1 2 N', @plateau)
+puts "Enter the initial position and co-ordinates of rover Rover_a. Example 1 2 N "
+@rover = Rover.new(gets.chomp, @plateau)
 @plateau.land_a_rover(@rover)
 
-puts "Giving instruction 'LMLMLMLMM' to the rover"
-@rover.process_instructions('LMLMLMLMM')
+puts "Enter the initial instruction for Rover_a. Example LMLMLMLMM"
+@rover.process_instructions(gets.chomp)
 
 puts "Rover_a's current position is #{@rover.current_position}"
 puts "========================================================="
  
-puts "Adding another rover rover_b with position 1 2 N to the same plateau"
-@rover = Rover.new('3 3 E', @plateau)
+puts "Adding another rover to the plateu."
+puts "Enter the initial position and co-ordinates of rover Rover_b. Example 3 3 E" 
+@rover = Rover.new(gets.chomp, @plateau)
 @plateau.land_a_rover(@rover)
 
-puts "Giving instruction 'MMRMMRMRRM' to the rover"
-@rover.process_instructions('MMRMMRMRRM')
+puts "Enter the initial instruction for Rover_b. Example MMRMMRMRRM"
+@rover.process_instructions(gets.chomp)
 
 puts "Rover_b's current position is #{@rover.current_position}"
 
